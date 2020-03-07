e0bc2a322fcf9ae7af6643c80d784dadd4fe09c3
whorst042 <whorst042@gmail.com>
2020-03-07 12:00:20 -0500

    JWT Branch

---
+++ build/resources/main/data.sql
@@ -0,0 +1,16 @@
+INSERT INTO user (username, password, enabled)
+    values(
+        'user',
+        'pass',
+        true);
+
+INSERT INTO user (username, password, enabled)
+    values(
+        'admin',
+        'pass',
+        true);
+
+INSERT INTO authorities (username, authority)
+    values ('user', 'ROLE_USER');
+INSERT INTO authorities (username, authority)
+    values ('admin', 'ROLE_ADMIN');
